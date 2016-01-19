a268a3bf5fbcc11e69b27674cd81cf089e99097f
Katie Sandford <katie@moo.com>
2016-01-19 22:24:26 +0000

    add api version

--- src/main/java/com/moo/wordpress/rest/api/Api.java
+++ src/main/java/com/moo/wordpress/rest/api/Api.java
@@ -10,9 +10,11 @@ import java.util.Map;
 
 /**
  * An interface for the MOO Wordpress API
+ * See http://wp-api.org/
  */
 public interface Api {
 
+    public final static String API_VERSION = "1.2.5";
     public final static String DATE_FORMAT = "yyyy-MM-dd'T'HH:mm";
 
     @GET("posts")
